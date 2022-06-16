5d9ca8059d04ef2e5aa5f5c2bd89f9451524e701
Renaldi <renaldisimamora67@gmail.com>
2022-06-16 11:47:28 +0700

    push repo

---
+++ ContosoCrafts.Website/Services/JsonFileProductService.cs
@@ -0,0 +1,32 @@
+﻿using ContosoCrafts.Website.Models;
+using System.Text.Json;
+
+namespace ContosoCrafts.Website.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "craft-products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
